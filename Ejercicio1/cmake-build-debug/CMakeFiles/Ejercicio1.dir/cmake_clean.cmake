9218e5e00f249860abd022988fd71ccf8b912b2f
Jorge Luis Vásquez <jorgeluisvasquez1996@gmail.com>
2019-11-12 07:22:13 -0500

    Ejercicios

---
+++ Ejercicio1/cmake-build-debug/CMakeFiles/Ejercicio1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejercicio1.dir/main.cpp.o"
+  "Ejercicio1"
+  "Ejercicio1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejercicio1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
